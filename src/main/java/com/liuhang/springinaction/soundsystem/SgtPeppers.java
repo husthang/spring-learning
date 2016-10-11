7c0456f723d6382ecb7308f1be4e8e020afe47e1
liuhang <981401658@qq.com>
2016-10-11 11:40:31 +0800

    sping test of stereo-autoconfig

--- src/main/java/com/liuhang/springinaction/soundsystem/SgtPeppers.java
+++ src/main/java/com/liuhang/springinaction/soundsystem/SgtPeppers.java
@@ -4,13 +4,15 @@ import org.springframework.stereotype.Component;
 
 /**
  * Created by liuhang on 16/10/10.
+ *gt. Pepper's Lonely Hearts Clue Band是披头士Beatles的一张著名专辑
  */
 @Component
-public class SgtPeppers  implements CompacDisc{
-    private String title="Sgt。 Pepper's Lonely Hearts Clue Band";
-    private String artist="The Beatles";
-    public void play(){
-        System.out.println("Playing"+title+"by"+artist);
+public class SgtPeppers implements CompacDisc {
+    private String title = "Sgt. Pepper's Lonely Hearts Clue Band";
+    private String artist = "The Beatles";
+
+    public void play() {
+        System.out.print("Playing " + title + " by " + artist);
     }
 
 }
--- src/test/java/com/liuhang/springinaction/soundsystem/CdPlayerTest.java
+++ src/test/java/com/liuhang/springinaction/soundsystem/CdPlayerTest.java
@@ -1,7 +1,11 @@
 package com.liuhang.springinaction.soundsystem;
 
 import static org.junit.Assert.*;
+
+import org.junit.Rule;
 import org.junit.Test;
+//import org.junit.contrib.java.lang.system.StandardOutputStreamLog;
+import org.junit.contrib.java.lang.system.SystemOutRule;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
@@ -11,12 +15,23 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
  * Created by liuhang on 16/10/10.
  */
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(classes=CdPlayerConfig.class)
+@ContextConfiguration(classes = CdPlayerConfig.class)
 public class CdPlayerTest {
+    @Rule
+    public final SystemOutRule systemOutRule = new SystemOutRule().enableLog();
     @Autowired
     private CompacDisc cd;
+    @Autowired
+    private MediaPlayer player;
+
     @Test
-    public void cdShouldNotBeNull(){
+    public void cdShouldNotBeNull() {
         assertNotNull(cd);
+    }
+
+    @Test
+    public void play() {
+        player.play();
+        assertEquals("Playing Sgt. Pepper's Lonely Hearts Clue Band by The Beatles", systemOutRule.getLog());
     }
 }
