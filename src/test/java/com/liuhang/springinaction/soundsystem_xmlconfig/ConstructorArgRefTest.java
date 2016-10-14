7ef4ef90cfea1e61669e14e9aec40415971856e9
liuhang <981401658@qq.com>
2016-10-14 16:25:56 +0800

    constructor arg test of xmlconfig

---
+++ src/test/java/com/liuhang/springinaction/soundsystem_xmlconfig/ConstructorArgRefTest.java
@@ -0,0 +1,32 @@
+package com.liuhang.springinaction.soundsystem_xmlconfig;
+
+import static org.junit.Assert.*;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.contrib.java.lang.system.SystemOutRule;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+/**
+ * Created by liuhang on 16/10/12.
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+/**
+ * class path的基目录 / 表示基目录
+ */
+@ContextConfiguration(locations = "/ConstructorArgRefTest.xml")
+public class ConstructorArgRefTest {
+    @Rule
+    public final SystemOutRule systemOutRule = new SystemOutRule().enableLog();
+    @Autowired
+    public MediaPlayer player;
+
+    @Test
+    public void play() {
+        player.play();
+        assertEquals("Playing Sgt. Pepper's Lonely Hearts Club Band by The Beatles", systemOutRule.getLog());
+    }
+}
